1ebd0c4f400b11c546cae1ac779bf3083f741166
beltrewilton <beltre.wilton@gmail.com>
2023-03-02 20:56:55 -0400

    new component, really looks not the best practices

---
+++ client/src/components/ProductPickedReadOnly.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useLayoutEffect } from "react";
+import { useAPI } from "../context/app-context";
+import { N_ } from "../util/Utils";
+
+export const ProductPickedReadOnly = () => {
+    console.log('ProductPickedReadOnly: rendered.')
+    const { sale, setSale, updateTSaleDetails } = useAPI()
+
+    return (
+        <div className="center-left-side-pay">
+            {sale.products.length > 0 && 
+            (sale.products.map((product, i) => (
+            <div key={i}  className={`product-picked`}
+                data-product-id={product.id} >
+                <div className="product-picked-label">
+                    <h3>{product.name}</h3>
+                </div>
+                <div className="dicount">
+                    {(product.discount != undefined && product.discount > 0) && <h3>  (-{N_((product.inventory.quantity_for_sale * product.price)  -  (product.inventory.quantity_for_sale * product.price_for_sale))}) or -{N_(product.discount_percent)}%  </h3>}
+                </div>
+                <div className="original-price">
+                    {(product.discount != undefined && product.discount > 0) && <h3>{N_(product.inventory.quantity_for_sale * product.price)}</h3>}
+                </div>
+                <div className="total-tags">
+                    <div className="quantity-tag">
+                        <h3>{product.inventory.quantity_for_sale} X {N_(product.price_for_sale)}</h3>
+                    </div>
+                    <div className="sub-tag">
+                        <h3>{N_(product.inventory.quantity_for_sale * product.price_for_sale)}</h3>
+                    </div>
+                </div>
+            </div>
+            )))}
+        </div>
+    );
+}
+
