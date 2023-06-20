00d91dd91806346d05f07b92261c413a7a4565bb
beltrewilton <beltre.wilton@gmail.com>
2023-06-19 21:49:14 -0400

    workflow testing automated reload # 2

--- run_in_cloud.sh
+++ run_in_cloud.sh
@@ -2,4 +2,6 @@ uvicorn server.main:app --host 0.0.0.0 --port 4443 \
 --ssl-keyfile /home/wilton/certs/retaily.app.key \
 --ssl-certfile /home/wilton/certs/1c55a1ede5e4705a.pem \
 --ssl-ca-certs /home/wilton/certs/gd_bundle-g2-g1.crt \
---reload
+--reload \
+--reload-include '*.jsx' \
+--reload-include '*.js'
