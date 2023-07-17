d9c3ee307f2ee81e5d65d63e4dd6b406c4129c8a
beltrewilton <beltre.wilton@gmail.com>
2023-07-17 10:11:37 -0400

    auto restart app

--- run_in_cloud.sh
+++ run_in_cloud.sh
@@ -5,7 +5,4 @@ conda activate retaily_env
 uvicorn server.main:app --host 0.0.0.0 --port 4443 \
 --ssl-keyfile /home/wilton/certs/retaily.app.key \
 --ssl-certfile /home/wilton/certs/1c55a1ede5e4705a.pem \
---ssl-ca-certs /home/wilton/certs/gd_bundle-g2-g1.crt \
---reload \
---reload-include '*.jsx' \
---reload-include '*.js'
+--ssl-ca-certs /home/wilton/certs/gd_bundle-g2-g1.crt
